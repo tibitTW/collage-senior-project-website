2e858446d65100013a9cc255ca4cc075c19b14ac
TibitFromTaiwan <jim870718@gmail.com>
2020-05-01 01:55:59 +0800

    .

--- Svg/script/script.js
+++ Svg/script/script.js
@@ -1,7 +1,11 @@
+// $('#hide-control').click(function () {
+//     $('.title').fadeOut(250, 'linear')
+//     $('.menu-bar').animate({
+//         opacity: 1,
+//         top: 0,
+//     }, 800)
+// })
 $('#hide-control').click(function () {
     $('.title').fadeOut(250, 'linear')
-    $('.menu-bar').animate({
-        opacity: 1,
-        top: 0,
-    }, 800)
+    $('.main-screen').fadeIn()
 })
