55f86e388650c9f8f512e63b757dbe670198c751
TibitFromTaiwan <jim870718@gmail.com>
2020-05-05 17:04:41 +0800

    .

--- script/script.js
+++ script/script.js
@@ -3,6 +3,30 @@ $('#hide-control').click(function () {
     $('.main').fadeIn()
 })
 
+window.onscroll = function () {
+    menubar = document.getElementById('menu-bar')
+    menu = document.getElementById('menu')
+    menuListTrigger = document.getElementsByClassName('menu-li-trigger')
+
+    if (window.pageYOffset > 80 || document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        menubar.style.height = '80px'
+        menubar.style.padding = '20px'
+        menu.style.lineHeight = '40px'
+        menu.style.marginRight = '.5vw'
+        for (var i = 0; i < 6; i++) {
+            menuListTrigger[i].style.margin = 'auto 1vw'
+        }
+    } else {
+        menubar.style.height = '120px'
+        menubar.style.padding = '25px'
+        menu.style.lineHeight = '70px'
+        menu.style.marginRight = '.5vw'
+        for (var i = 0; i < 6; i++) {
+            menuListTrigger[i].style.margin = 'auto 2.2vw'
+        }
+    }
+}
+
 // $(document).on('click', 'a[href^="#"]', function (event) {
 //     event.preventDefault()
 
@@ -11,17 +35,17 @@ $('#hide-control').click(function () {
 //     }, 120)
 
 // })
-$('a').on('click', function (event) {
-    if (this.hash !== '') {
-        event.preventDefault()
-    }
+// $('a').on('click', function (event) {
+//     if (this.hash !== '') {
+//         event.preventDefault()
+//     }
 
-    var hash = this.hash
+//     var hash = this.hash
 
-    $('html, body').animate({
-        scrolltop: ($(hash).offset().top - 100)
-    }, 120, function () {
-        window.location.hash = hash
-    })
+//     $('html, body').animate({
+//         scrolltop: ($(hash).offset().top - 100)
+//     }, 120, function () {
+//         window.location.hash = hash
+//     })
 
-})
+// })
