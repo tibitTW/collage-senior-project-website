df0ca3e3db6a0ceba2f9e28daa898ec5705fee7c
TibitFromTaiwan <jim870718@gmail.com>
2020-05-05 13:05:21 +0800

    大優化

---
+++ script/script.js
@@ -0,0 +1,27 @@
+$('#hide-control').click(function () {
+    $('.svg').fadeOut(250, 'linear')
+    $('.main').fadeIn()
+})
+
+// $(document).on('click', 'a[href^="#"]', function (event) {
+//     event.preventDefault()
+
+//     $('html, body').animate({
+//         scrolltop: $($.attr(this, 'href')).offset().top
+//     }, 120)
+
+// })
+$('a').click(function (event) {
+    if (this.hash !== '') {
+        event.preventDefault()
+    }
+
+    var hash = this.hash
+
+    $('html, body').animate({
+        scrolltop: ($(hash).offset().top - 100)
+    }, 120, function () {
+        window.location.hash = hash
+    })
+
+})
