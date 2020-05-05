9251f83a98409058599cdb0e4a0202551074abcc
TibitFromTaiwan <jim870718@gmail.com>
2020-05-05 12:03:29 +0800

    .

--- script/script.js
+++ script/script.js
@@ -1,29 +1,27 @@
-$(document).ready(function () {
-    $('#hide-control').click(function () {
-        $('.svg').fadeOut(250, 'linear')
-        $('.main').fadeIn()
+$('#hide-control').click(function () {
+    $('.svg').fadeOut(250, 'linear')
+    $('.main').fadeIn()
+})
+
+// $(document).on('click', 'a[href^="#"]', function (event) {
+//     event.preventDefault()
+
+//     $('html, body').animate({
+//         scrolltop: $($.attr(this, 'href')).offset().top
+//     }, 120)
+
+// })
+$('a').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault()
+    }
+
+    var hash = this.hash
+
+    $('html, body').animate({
+        scrolltop: ($(hash).offset().top - 100)
+    }, 120, function () {
+        window.location.hash = hash
     })
 
-    // $(document).on('click', 'a[href^="#"]', function (event) {
-    //     event.preventDefault()
-
-    //     $('html, body').animate({
-    //         scrolltop: $($.attr(this, 'href')).offset().top
-    //     }, 120)
-
-    // })
-    $('a').on('click', function (event) {
-        if (this.hash !== '') {
-            event.preventDefault()
-        }
-
-        var hash = this.hash
-
-        $('html, body').animate({
-            scrolltop: ($(hash).offset().top - 100)
-        }, 120, function () {
-            window.location.hash = hash
-        })
-
-    })
 })
